880ba28d8cc6aed49d1f1f056aff7aafc12a700e
Efrem4ik <efrem4ik12@mail.ru>
2019-11-08 14:53:25 +0300

    update gulp-start

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,6 @@ gulp.task('script', function () {
       'node_modules/slick-carousel/slick/slick.js',
       'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
       'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-      'node_modules/wow.js/dist/wow.js',
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
@@ -50,7 +49,6 @@ gulp.task('style', function () {
       'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
       'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
       'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
-      'node_modules/animate.css/animate.css',
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
